efbeb587b579f0a515a03c581a49c23656c23f83
Thiago Falcão <thifalcao01@gmail.com>
2023-10-24 14:04:35 -0300

    Update backend URL

--- src/components/Cart/CartPage.jsx
+++ src/components/Cart/CartPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 
 import "./CartPage.css";
+import config from "../../config.json";
 import remove from "../../assets/remove.png";
 import Table from "../Common/Table.jsx";
 import QuantityInput from "../SingleProduct/QuantityInput";
@@ -38,7 +39,7 @@ const CartPage = () => {
     <section className="align_center cart_page">
       <div className="align_center user_info">
         <img
-          src={`http://localhost:5000/profile/${user?.profilePic}`}
+          src={`${config.backendURL}/profile/${user?.profilePic}`}
           alt="user profile"
         />
         <div>
